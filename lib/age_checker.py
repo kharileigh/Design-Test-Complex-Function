c5e0b3e5e1a30213310feb40e3dcd1deac591736
kharileigh <kharileighmiller@gmail.com>
2024-08-12 16:30:41 +0100

    User below age 16, access denied -- test passed

--- lib/age_checker.py
+++ lib/age_checker.py
@@ -1,6 +1,27 @@
-
+from datetime import datetime, date
 
 def age_checker(dob):
-    
+
+    # ------ NO INPUT 
     if dob == "":
         raise Exception("No text entered, please enter you date of birth: YYYY-MM-DD")
+
+    # ------ CALCULATE USER'S AGE
+    # today's date - get date - convert to str
+    now = str(date.today())
+
+    # parse dob & today into a datetime object -- 1st arg must be string, 2nd arg desired format
+    today = datetime.strptime(now, "%Y-%m-%d").date()
+    birthday = datetime.strptime(dob, "%Y-%m-%d").date()
+
+    # difference between current date and user's birthday
+    age = today - birthday
+
+    # convert age into years -- round to nearest integer
+    current_age = round(age.days / 365)
+    
+    
+
+    # -------- GIVEN INPUT -- meets age requirements, access granted -- doesn't meet, access denied
+    if current_age < 16:
+        return f"Access denied, your current age is: {current_age} years and the required age is 16"
--- tests/test_age_checker.py
+++ tests/test_age_checker.py
@@ -10,3 +10,11 @@ def test_age_checker_no_input():
     error_message = str(err.value)
     
     assert error_message == "No text entered, please enter you date of birth: YYYY-MM-DD"
+
+
+# ------ ACCESS DENIED
+def test_age_checker_denied():
+
+    assert age_checker("2010-11-04") == "Access denied, your current age is: 14 years and the required age is 16"
+
+
